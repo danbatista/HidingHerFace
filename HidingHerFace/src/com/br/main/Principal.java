e34937cbc9750f30b898802340b17a565064cc50
rafaelguinho <rafael.monteiro user @ unimedcaruaru.com.br>
2014-05-07 16:16:11 -0300

    Início do projeto

---
+++ HidingHerFace/src/com/br/main/Principal.java
@@ -0,0 +1,71 @@
+package com.br.main;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.highgui.Highgui;
+import org.opencv.objdetect.CascadeClassifier;
+
+import com.br.model.PropriedadesFace;
+import com.br.service.ServiceDesfoqueImagem;
+import com.br.service.ServiceCorteImagem;
+import com.br.service.ServiceExtracaoFacesImagem;
+import com.br.service.ServiceSobreposicaoImagem;
+import com.br.util.Util;
+
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+		CascadeClassifier cascadeClassifier = new CascadeClassifier("C:/Users/rafael.monteiro/workspace/applet/RecFacial/src/com/shekhar/facedetection/haarcascade_frontalface_alt.xml");
+
+		Mat mat = Highgui
+				.imread("C:/Users/rafael.monteiro/Desktop/fundo3.jpg");
+		
+		//faz a detecção das faces
+		ServiceExtracaoFacesImagem serviceExtractFaces = new ServiceExtracaoFacesImagem();
+		MatOfRect matOfRect = serviceExtractFaces.detectarFaces(cascadeClassifier, mat);
+		
+		//obtem os dados de onde estão as faces (altura, largura, posição x e y)
+		List<PropriedadesFace> propsFaces = serviceExtractFaces.obterDadosFaces(matOfRect);
+		
+		BufferedImage imagemCorteDesfoque = Util.converterParaImage(mat);
+		
+		//desfoca a imagem
+		ServiceDesfoqueImagem serviceBlur = new ServiceDesfoqueImagem();
+		imagemCorteDesfoque = serviceBlur.DesfocarImagem(imagemCorteDesfoque);
+		
+		//corta os rostos da imagem desfocada, 
+		ServiceCorteImagem serviceCrop = new ServiceCorteImagem();
+		propsFaces = serviceCrop.CortarImagem(propsFaces, imagemCorteDesfoque);
+		
+		ServiceSobreposicaoImagem serviceOverlay = new ServiceSobreposicaoImagem();
+		
+		//obtem toda a imagem se efeitos
+		BufferedImage imagemSemEfeitos = Util.converterParaImage(mat);
+		
+		//"cola" os rostos desfocados sobre a imagem original
+		imagemCorteDesfoque = serviceOverlay.juntarImagens(propsFaces, imagemSemEfeitos);
+		
+		File outputfile = new File("saved.png");
+		
+	    try {
+			ImageIO.write(imagemCorteDesfoque, "png", outputfile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ HidingHerFace/src/com/br/service/ServiceDesfoqueImagem.java
@@ -0,0 +1,30 @@
+package com.br.service;
+
+import java.awt.image.BufferedImage;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import com.br.util.Util;
+
+public class ServiceDesfoqueImagem {
+	
+	public BufferedImage DesfocarImagem(BufferedImage imagem){
+		Mat mat = Util.converterParaMat(imagem);
+		mat = DesfocarImagem(mat);
+		
+		return Util.converterParaImage(mat);
+	}
+	
+	
+	private Mat DesfocarImagem(Mat image){
+			      
+		Mat destination = new Mat(image.rows(),image.cols(),image.type());
+			      
+		Imgproc.GaussianBlur(image, destination,new Size(91,91), 0);
+		
+		return destination;
+	}
+	
+}
